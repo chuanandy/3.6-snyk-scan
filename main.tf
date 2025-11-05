c9ce4392a40262e178e9433c7e75c3651ffaf0a5
chuanandy <chuanandy@gmail.com>
2025-11-06 00:15:14 +0800

    update the code for lambda functions  & check

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_hello_world_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Effect = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_function"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello-world-lambda"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "main.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
